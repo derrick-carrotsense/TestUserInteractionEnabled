421101b874840f366cb8b47f94e13b00995915da
Derrick Thai <derrick@carrotsense.com>
2017-09-22 11:00:42 -0700

    Initial Commit

---
+++ Test User Interaction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test User Interaction
+//
+//  Created by Derrick Thai on 9/22/17.
+//  Copyright © 2017 Derrick Thai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
